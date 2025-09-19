46f8cc6411c0e40885ac44174d4b000d895c9618
KrishCodesw <krishjain.w@gmail.com>
2025-09-20 02:28:22 +0530

    id

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Civic Reporter",
+  title: "JanSamvaad",
   description: "Crowdsourced civic issue reporting and resolution",
 };
 
